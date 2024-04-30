2c7b93b2d81220d73c5f90588f7cfbab028c3045
LukaMelinc <luka.melinc1@gmail.com>
2024-04-30 23:12:07 +0200

    functions added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import PIL as Image
+
 class QOIEncoder:
     def __init__(self, data, width, height, channels):
         self.data = data
@@ -24,4 +26,40 @@ class QOIEncoder:
 
         previous_pixels = [0] * self.channels
 
-        for i in range(0, len(data), st_kanalov):
+        for i in range(0, len(self.data), self.channels):
+            pixel = self.data[i:i + self.channels]
+            if pixel == previous_pixels:
+                self.encoded.append(0b11000000) # QOI command for a repeated pixel
+            else:
+                self.encoded.extend(pixel)  # raw data si directly appended to self.encoded bytearray
+            previous_pixels = pixel
+
+        self.encoded.append(0b00000000)     # signalises the end of datastream
+        return self.encoded
+    
+    def save_to_file(self, file_path):
+        with open(file_path, "wb") as file:
+            file.write(self.encoded)
+
+def open_image(file_path):
+    "Opens image and converts it into RGB/RGBA format"
+    img = Image.open(file_path)
+    if img.mode not in ["RGB", "RGBA"]:
+        img = img.convert("RGBA")
+    return img
+
+def process_image(file_path, output_path):
+    with open(f"kodim14.png", "rb") as f:
+        img = f.read()
+    
+    if img is not None:
+        data = list(img.getdata())
+        flat_data = [channel for pixel in data for channel in pixel]  # Flatten the list of tuples
+        encoder = QOIEncoder(flat_data, img.width, img.height, len(img.mode))
+        encoded_image = encoder.encode()
+        encoder.save_to_file(output_path)
+
+
+process_image("/Users/lukamelinc/Desktop/Faks/MAG_1_letnik/2_semester/Informacije in Kodi/LAB/Projekt/archive/kodim01.png", "/Users/lukamelinc/Desktop/Faks/MAG_1_letnik/2_semester/Informacije in Kodi/LAB/Projekt")
+
+   
